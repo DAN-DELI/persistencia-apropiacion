a85df789f218f3d8fbf106b0d53479c41d2ac341
Jhon Bueno <jhonbuenoa@gmail.com>
2026-03-18 21:06:35 -0500

    feat: agregar categorias)

---
+++ src/data/categories-data.js
@@ -0,0 +1,13 @@
+// comentario: El nombre del archivo es categories-data.js. ya que si se usaba "categories.data.js", se generaba un error como si no se encontrase este archivo.
+
+const categoriesData = [
+  { id: 1, name: "Sistemas y Equipos" },
+  { id: 2, name: "Periféricos" },
+  { id: 3, name: "Hardware y Componentes" },
+  { id: 4, name: "Conectividad y Redes" },
+  { id: 5, name: "Mobiliario" },
+  { id: 6, name: "Dispositivos Móviles" },
+  { id: 7, name: "Audio" },
+];
+
+export default categoriesData;
