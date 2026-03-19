8405202f994269e958313f2d6ebf7ef54b97f04f
Jhon Bueno <jhonbuenoa@gmail.com>
2026-03-18 21:04:47 -0500

    add: agregar controladores de categorías (obtener, crear, actualizar, eliminar)

---
+++ src/controllers/category.controller.js
@@ -0,0 +1,115 @@
+
+import { CategoryModel } from '../data./categories.data';
+
+const getAllCategories = (req, res) => {
+  const categories = CategoryModel.findAll();
+  res.status(200).json({
+    success: true,
+    message: "Lista de categorías",
+    data: categories,
+    errors: [],
+  });
+};
+
+
+const getCategoryById = (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = CategoryModel.findById(Number(id));
+    // Validamos si la categoría existe
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: `Categoria con ID ${id} no encontrado`,
+        data: [],
+        errors: [],
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Categoria encontrada correctamente",
+      data: category,
+      errors: [],
+    });
+
+  } catch (error) { 
+    res.status(500).json({
+      success: false,
+      message: "Error al procesar la búsqueda",
+      data: [],
+      errors: [],
+    });
+  }
+};
+
+
+const createCategory = (req, res) => {
+  const { name } = req.body;
+  // Validación simple
+  if (!name) {
+    return res.status(400).json({
+      success: false,
+      message: "Nombre es obligatorio",
+      data: [],
+      errors: [],
+    });
+  }
+
+  const newCategory = CategoryModel.create({ name });
+  res.status(201).json({
+    success: true,
+    message: "Categoria creado correctamente",
+    data: newCategory,
+    errors: [],
+  });
+};
+
+const updateCategory = (req, res) => {
+  const { id } = req.params;
+  const updatedCategory = CategoryModel.update(Number(id), req.body);
+  if (!updatedCategory) { 
+    return res.status(404).json({
+      success: false,
+      message: `Categoria con ID ${id} no encontrado`,
+      data: [],
+      errors: [],
+    });
+  }
+  res.status(200).json({
+    success: true,
+    message: "Categoria actualizada correctamente",
+    data: updatedCategory,
+    errors: [],
+  });
+};
+
+
+const deleteCategory = (req, res) => {
+  try {
+    const { id } = req.params;
+    const isDeleted = CategoryModel.delete(Number(id));
+    if (!isDeleted) {
+      return res.status(404).json({
+        success: false,
+        message: `No se pudo eliminar: Categoria con ID ${id} no encontrado`,
+        data: [],
+        errors: [],
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Categoria eliminada correctamente",
+      data: [],
+      errors: [],
+    });    
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Error al intentar eliminar la categoria`,
+      data: [],
+      errors: [],
+    });
+  } 
+}
+
+export { getAllCategories, getCategoryById, createCategory, updateCategory, deleteCategory };
